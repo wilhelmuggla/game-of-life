e1f6b26280ab4dd3d457419569fca63f809f35c7
Wilhelm Uggla <willeuggla@gmail.com>
2020-08-23 20:18:15 +0200

    Updates

---
+++ precache-manifest.1cf1928effd6ba39b7951186170cda7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb2140f9baaeec9dc44f8d73ae88909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3dbbffc998db6a12293",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "434e6c7dca8a01b00d77",
+    "url": "/static/css/main.2521cd0e.chunk.css"
+  },
+  {
+    "revision": "f3dbbffc998db6a12293",
+    "url": "/static/js/2.bea216aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bea216aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434e6c7dca8a01b00d77",
+    "url": "/static/js/main.bd75610a.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  }
+]);
